f398e9f9871c600268624469c178cf2c7ef850b4
melvin <ianndiormel@gmail.com>
2025-04-03 19:15:35 +0100

    validate id param

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,17 +1,21 @@
-import { Controller, Get, Post, Put, Param
+import { Controller, Get, Post, Put, Param, Query, Body, Headers, Ip, ParseIntPipe
 
  } from '@nestjs/common';
 
 @Controller('users')
 export class UsersController {
-    @Get('/:id')
-    public getUsers (@Param() Param: any){
-        console.log(Param)
+    @Get('{/:id}')
+    public getUsers (@Param('id', ParseIntPipe) id: number | undefined, @Query('limit') limit: any){
+        console.log(id)
+        console.log(limit)
         return 'requested for get form the users endpoint'
     }
 
     @Post()
-    public creatUsers (){
+        public creatUsers(@Body() Body: any, @Headers() headers: any, @Ip() ip: any) {
+        console.log(Body)
+        console.log(headers)
+        console.log(ip)
         return 'requested for post form the users endpoint'
-    }
+}
 }
