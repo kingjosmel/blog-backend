10b645e8e6e4b7ff94beeaad8ff1cc14c3e3f0dc
melvin <ianndiormel@gmail.com>
2025-04-07 15:38:44 +0100

    created user service provider and injected it to the module and controllers

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -4,9 +4,14 @@ import { Controller, Get, Post, Put, Param, Query, Body, Headers, Ip, ParseIntPi
 import { CreateUsersDto } from './dtos/create-users.dto';
 import GetUsersParamDto from './dtos/get-users-param.dto';
 import { PatchUserDto } from './dtos/patch-users.dto';
-
+import { UserService } from './providers/users.service';
 @Controller('users')
 export class UsersController {
+    constructor(
+        private readonly UserService : UserService
+    ){
+
+    }
     @Get('{/:id}')
     public getUsers (@Param() getUsersParamDto: GetUsersParamDto, @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number, @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number){
         console.log(getUsersParamDto)
